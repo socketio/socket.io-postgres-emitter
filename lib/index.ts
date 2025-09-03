82ed109e608bdea5425341097d5edb09a1c88e84
Niwa <niwaisme@proton.me>
2025-09-03 16:47:57 +0700

    fix: handle single quota in message. resulting in error 'scanner_yyerror'

--- lib/index.ts
+++ lib/index.ts
@@ -353,6 +353,36 @@ export class BroadcastOperator<
       flags
     );
   }
+  // Ported from PostgreSQL 9.2.4 source code in src/interfaces/libpq/fe-exec.c
+  escapeIdentifier(str: any) {
+    return '"' + str.replace(/"/g, '""') + '"';
+  }
+
+  // Ported from PostgreSQL 9.2.4 source code in src/interfaces/libpq/fe-exec.c
+  escapeLiteral(str: any) {
+    var hasBackslash = false;
+    var escaped = "'";
+
+    for (var i = 0; i < str.length; i++) {
+      var c = str[i];
+      if (c === "'") {
+        escaped += c + c;
+      } else if (c === "\\") {
+        escaped += c + c;
+        hasBackslash = true;
+      } else {
+        escaped += c;
+      }
+    }
+
+    escaped += "'";
+
+    if (hasBackslash === true) {
+      escaped = " E" + escaped;
+    }
+
+    return escaped;
+  }
 
   private async publish(document: any) {
     document.uid = EMITTER_UID;
@@ -407,7 +437,9 @@ export class BroadcastOperator<
       attachmentId,
     });
     this.emitter.pool.query(
-      `NOTIFY "${this.emitter.channel}", '${headerPayload}'`
+      `NOTIFY ${this.escapeIdentifier(
+        this.emitter.channel
+      )}, ${this.escapeLiteral(payload)}`
     );
   }
 
